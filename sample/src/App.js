68a300569405b19982bcd11c8521c460112c9392
Hung Tran Van <tvhung1>
2023-07-07 15:55:12 +0700

    Implement sample app and update ckeditor

---
+++ sample/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/scss/bootstrap.scss';
+import { FieldEditor } from 'itm-ui';
+import 'itm-ui/scss/itm-ui.scss';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App p-5">
+      <FieldEditor />
+    </div>
+  );
+}
+
+export default App;
