629d7e3dd0ca5bdc9060ea5c0001e377847255e2
Văn Hùng <62246953+vhungitm@users.noreply.github.com>
2023-07-26 11:02:48 +0700

    Create App.tsx

---
+++ sample/src/App.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/scss/bootstrap.scss';
+import 'itm-ui/scss/itm-ui.scss';
+import * as React from 'react';
+
+const App = () => {
+  return <div className="p-5"></div>;
+};
+
+export default App;
